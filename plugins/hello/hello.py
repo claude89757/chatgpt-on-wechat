c8ba9d4c05019416569cfda6dc3cd5b1e0094538
claude89757 <claudexie@outlook.com>
2023-11-21 13:27:26 +0800

    fix

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -48,7 +48,7 @@ class Hello(Plugin):
         if e_context["context"].type == ContextType.PATPAT:
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+            e_context["context"].content = f"请你随机介绍一个简短的网球的小知识"
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
