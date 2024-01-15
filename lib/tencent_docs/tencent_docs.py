a7d0920c8c0b743830d25900d5573747a99c4bf8
claude89757 <claudexie@outlook.com>
2024-01-15 11:15:40 +0800

    fix

--- lib/tencent_docs/tencent_docs.py
+++ lib/tencent_docs/tencent_docs.py
@@ -184,7 +184,7 @@ class TencentDocs(object):
             if data['row'] >= 2:
                 cell_data = data['textValues'][1]
                 time_slot = time_slots[data['row']-2]
-                if "已过期" in cell_data:
+                if "已过期" in cell_data or "赞助" in cell_data:
                     pass
                 else:
                     court_name_list = []
@@ -207,7 +207,7 @@ class TencentDocs(object):
             if data['row'] >= 2:
                 cell_data = data['textValues'][2]
                 time_slot = time_slots[data['row'] - 2]
-                if "已过期" in cell_data:
+                if "已过期" in cell_data or "赞助" in cell_data:
                     pass
                 else:
                     court_name_list = []
