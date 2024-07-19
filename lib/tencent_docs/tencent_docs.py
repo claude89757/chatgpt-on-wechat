c499b6ef7c5b9dc1ee764066dd4d4891676c5a41
claude89757 <claudexie@outlook.com>
2024-07-19 15:31:39 +0800

    add 华侨城

--- lib/tencent_docs/tencent_docs.py
+++ lib/tencent_docs/tencent_docs.py
@@ -280,7 +280,7 @@ class TencentDocs(object):
                             court_name_list.append(str(line.split()[0]).strip())
 
                     for court_name in court_name_list:
-                        if court_name in ["大沙河", "金地威新", "香蜜体育", "香蜜电话", "深圳湾", "黄木岗"]:
+                        if court_name in ["大沙河", "金地威新", "香蜜体育", "香蜜电话", "深圳湾", "黄木岗", "华侨城"]:
                             court_key = f"【{court_name}】{weekday}({date_str})空场"
                             if court_free_time_infos.get(court_key):
                                 court_free_time_infos[court_key].append(time_slot)
