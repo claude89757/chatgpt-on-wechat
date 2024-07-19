282e50e3f080ab2eb7db56177579109d2a11f4cc
claude89757 <claudexie@outlook.com>
2024-07-19 16:13:46 +0800

    fix

--- lib/tencent_docs/tencent_docs.py
+++ lib/tencent_docs/tencent_docs.py
@@ -253,7 +253,7 @@ class TencentDocs(object):
                       ['08:00', '09:00'],
                       ['07:00', '08:00'], ]
 
-
+        push_courts = ["大沙河", "金地威新", "香蜜体育", "香蜜电话", "深圳湾", "黄木岗", "华侨城", "网羽中心"]
         # 获取当前日期
         weekday = datetime.datetime.today().weekday()
         if weekday == 5:
@@ -280,7 +280,7 @@ class TencentDocs(object):
                             court_name_list.append(str(line.split()[0]).strip())
 
                     for court_name in court_name_list:
-                        if court_name in ["大沙河", "金地威新", "香蜜体育", "香蜜电话", "深圳湾", "黄木岗", "华侨城"]:
+                        if court_name in push_courts:
                             court_key = f"【{court_name}】{weekday}({date_str})空场"
                             if court_free_time_infos.get(court_key):
                                 court_free_time_infos[court_key].append(time_slot)
@@ -309,7 +309,7 @@ class TencentDocs(object):
                             court_name_list.append(str(line.split()[0]).strip())
 
                     for court_name in court_name_list:
-                        if court_name in ["大沙河", "金地威新", "香蜜体育", "香蜜电话", "深圳湾", "黄木岗"]:
+                        if court_name in push_courts:
                             court_key = f"【{court_name}】{weekday}({date_str})空场"
                             if court_free_time_infos.get(court_key):
                                 court_free_time_infos[court_key].append(time_slot)
